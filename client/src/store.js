031a3397c906e0b032792ef9bdb02d9c6355c549
Miles <milesdpratt@gmail.com>
2018-07-16 14:36:12 -0400

    Update redux store to only load redux extension when using chrome.

--- client/src/store.js
+++ client/src/store.js
@@ -7,12 +7,15 @@ const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-	rootReducer,
-	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.navigator.userAgent.includes("Chrome")
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose
+  )
 );
 
 export default store;
